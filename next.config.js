5b8c1a9d296bb38d0c107922d2ee21917ffbafe6
RUVNE <dian.galleon@gmail.com>
2023-05-19 06:09:30 +0800

    fix build failed cause layer

--- next.config.js
+++ next.config.js
@@ -2,9 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: function (config, options) {
-    config.experiments = { asyncWebAssembly: true };
+    config.experiments = {
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+      layers: true 
+    };
     return config;
-  }
+  },
 }
 
 module.exports = nextConfig
